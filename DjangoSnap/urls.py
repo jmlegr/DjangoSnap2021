5f5555ad7c29afaecfb92144f0bd3b2622c1a6fe
JML <jmlegr@gmail.com>
2017-12-06 17:26:49 +0100

    premier test

--- DjangoSnap/urls.py
+++ DjangoSnap/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from django.urls.conf import include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('snap/',include('snap.urls')),
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ snap/urls.py
@@ -0,0 +1,12 @@
+'''
+Created on 4 déc. 2017
+
+@author: duff
+'''
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('test',views.testsnap),
+    ]
