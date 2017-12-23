6acc5e1e69944c0ebc2e2cd55d16b4a327044a0c
JML <jmlegr@gmail.com>
2017-12-23 13:21:08 +0100

    ajout restframework et suppression Question

--- snap/admin.py
+++ snap/admin.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question, ActionProgrammation, Bounds, DroppedBlock, Inputs, \
+from .models import ActionProgrammation, Bounds, DroppedBlock, Inputs, \
         Point, Document
 
 
 class DocumentAdmin(admin.ModelAdmin):
     readonly_fields=['uploaded_at',]
     
-admin.site.register(Question)
+
 admin.site.register(ActionProgrammation)
 admin.site.register(Bounds)
 admin.site.register(DroppedBlock)
--- snap/models.py
+++ snap/models.py
@@ -1,10 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+
     
 class InfoReceived(models.Model):
     block_id=models.IntegerField()
