2eac97cf9710ba2a266ec87931335036414b5a9a
JML <jmlegr@gmail.com>
2018-10-12 12:46:34 +0200

    ajout de l'image sur snapshot

--- visualisation_boucles/serializers.py
+++ visualisation_boucles/serializers.py
@@ -5,7 +5,7 @@ Created on 7 oct. 2018
 '''
 from rest_framework import serializers
 from snap.models import ProgrammeBase,EvenementENV, EvenementEPR, EvenementSPR,\
-    Evenement
+    Evenement, SnapSnapShot
 from snap import serializers as snapserializers
 class ProgrammeBaseSerializer(serializers.ModelSerializer):
     class Meta:
@@ -20,17 +20,28 @@ class EvenementENVSerializer(serializers.ModelSerializer):
         fields='__all__'
         #read_only_fields=('evenement',)
 
-class SimpleENVSerializer(serializers.ModelSerializer):
+class SimpleSnapShotSerializer(serializers.ModelSerializer):
+    image=serializers.ImageField()
+    class Meta:
+        model=SnapSnapShot
+        exclude=('evenement',)
+        
+class SimpleENVSerializer(serializers.ModelSerializer):   
     class Meta:
         model=EvenementENV
         exclude=('evenement',)
     
-class SimpleEPRSerializer(serializers.ModelSerializer):
+class SimpleEPRSerializer(serializers.ModelSerializer): 
     class Meta:
         model=EvenementEPR
         exclude=('evenement',)
 
 class SimpleSPRSerializer(serializers.ModelSerializer):
+    nb=serializers.SerializerMethodField()
+    def get_nb(self,obj):
+        return "SSS %s next %s" % (obj.blockId,obj.nextBlockId)
+    
+    
     class Meta:
         model=EvenementSPR
         exclude=('evenement',)
@@ -38,6 +49,13 @@ class SimpleEvenementSerializer(serializers.ModelSerializer):
     evenementepr=SimpleEPRSerializer(many=True)
     evenementspr=SimpleSPRSerializer(many=True)
     environnement=SimpleENVSerializer(many=True)
+    
+    image=SimpleSnapShotSerializer(many=True,read_only=True)  
+    isSpr=serializers.SerializerMethodField()
+    
+    def get_isSpr(self,obj):
+        return obj.type=="SPR"            
+    
     class Meta:
         model=Evenement
         fields='__all__'
