cdecb557355a43cb656bded4f6434b6454ceda99
JML <jmlegr@gmail.com>
2018-10-22 23:20:49 +0200

    beautify...

--- visualisation_boucles/static/visualisation_boucles/js/xsend.js
+++ visualisation_boucles/static/visualisation_boucles/js/xsend.js
@@ -1,24 +1,31 @@
-let data={d:"ok",j:true}
-let url="sessions/visualise/"
-    
-var xsend=function(url,csrf_token,data,method="GET") {
- return fetch(url, {
-        method: method, // *GET, POST, PUT, DELETE, etc.
-        mode: "cors", // no-cors, cors, *same-origin
-        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: "same-origin", // include, same-origin, *omit
-        headers: {
-            "Content-Type": "application/json; charset=utf-8",
-            'X-CSRFToken': csrf_token,
-            // "Content-Type": "application/x-www-form-urlencoded",
-        },
-        redirect: "follow", // manual, *follow, error
-        referrer: "no-referrer", // no-referrer, *client
-        body: JSON.stringify(data), // body data type must match "Content-Type" header
-    })
-    .then(response => response.json())
-    //.then(response => console.log('Success:', response,JSON.stringify(response)))
-.catch(error => console.error('Erroddr:', error));; // parses response to JSON
+let data = {
+    d: "ok",
+    j: true
+}
+let url = "sessions/visualise/"
+
+var xsend = function (url, csrf_token, data, method = "GET") {
+    return fetch(url, {
+            method: method, // *GET, POST, PUT, DELETE, etc.
+            mode: "cors", // no-cors, cors, *same-origin
+            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: "same-origin", // include, same-origin, *omit
+            headers: {
+                "Content-Type": "application/json; charset=utf-8",
+                'X-CSRFToken': csrf_token,
+                // "Content-Type": "application/x-www-form-urlencoded",
+            },
+            redirect: "follow", // manual, *follow, error
+            referrer: "no-referrer", // no-referrer, *client
+            body: JSON.stringify(data), // body data type must match "Content-Type" header
+        })
+        .then(response => response.json())
+        //.then(response => console.log('Success:', response,JSON.stringify(response)))
+        .catch(error => console.error('Erroddr:', error));; // parses response to JSON
 }
 
-export { data, url, xsend }
+export {
+    data,
+    url,
+    xsend
+}
