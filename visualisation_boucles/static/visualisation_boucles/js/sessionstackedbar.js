cdf01f51f3f6c01fb8d4365d135ffef1b93617c1
JML <jmlegr@gmail.com>
2019-01-02 12:46:14 +0100

    ajout legende et selection

--- visualisation_boucles/static/visualisation_boucles/js/sessionstackedbar.js
+++ visualisation_boucles/static/visualisation_boucles/js/sessionstackedbar.js
@@ -8,9 +8,9 @@ var initSessionStackedBarChart = {
         boucles=config.boucles, //tableau des premières boucles trouvées par session
         sep=config.separator || '_', //separateur type_datatype passé en key
         liste=d3.map(config.liste,d=>d.session_key), //liste des infos de session, map de clef session_key
-        margin = {top: 20, right: 20, bottom: 30, left: 50},
+        margin = {top: 20, right: 120, bottom: 30, left: 50},
         parseDate = d3.timeParse("%m/%Y"),
-        width = 960 - margin.left - margin.right,
+        width = d3Ele.node().getBoundingClientRect().width - margin.left - margin.right,
         height = 500 - margin.top - margin.bottom,
         xScale = d3.scaleBand().range([0, width]).padding(0.1),
         yScale = d3.scaleLinear().range([height, 0]),
@@ -70,7 +70,8 @@ var initSessionStackedBarChart = {
                         return p;
                     }
             )
-    var zdata=countBySession.top(Infinity).map(d=>{
+            
+    var zdata=()=> countBySession.top(Infinity).map(d=>{
         var a={};
       a.session=d.key
       for (var v in d.value) {
@@ -78,72 +79,118 @@ var initSessionStackedBarChart = {
       }
       return a
       })
+      
     //console.log('map',data,zdata,stackKey)
     //console.log('boucles',boucles)
-        var stack = d3.stack()
-            .keys(stackKey.sort())
-            .order(d3.stackOrderNone)
-            // .order(d3.stackOrderDescending)
-            .offset(d3.stackOffsetNone);
-    
-        var layers= stack(zdata)
-            //data.sort(function(a, b) { return b.s - a.s; });
-            xScale.domain(zdata.map(function(d) { return d.session; }));
-        //console.log("layers",layers)
-        //recherche du y max
-        var maxi=0
-        layers.forEach(l=>{let m=d3.max(l,function(d){return d[1]?d[1]:0}); if (m>maxi) maxi=m})
-        yScale.domain([0, maxi]).nice();
-        var layer = svg.selectAll(".layer")
-            .data(layers)
-            .enter().append("g")
-            .attr("class", "layer")
-            .style("fill", function(d, i) { return color(d.key); })
-      
-          var rectSel=layer.selectAll("g.evt")
-              .data(function(d) { return d; })
-              .enter()
-              .append("g").attr("class",'evt')
-              .classed('noboucle',d=>boucles[d.data.session]==null)
-              .classed('boucle',d=>boucles[d.data.session]!=null)
-          rectSel.append("rect")
-                //.classed('noboucle',d=>boucles[d.data.session]==null)
-                //.classed('boucle',d=>boucles[d.data.session]!=null)
-              .attr("x", function(d) { return xScale(d.data.session); })
-              .attr("y", function(d) { return yScale(d[1]); })
-              .attr("height", function(d) { return yScale(d[0]) - yScale(d[1]); })
-              .attr("width", xScale.bandwidth())
-          rectSel.append("line")
-              .attr("class","evtline")
-              .attr("x1",d=>xScale(d.data.session))
-              .attr("y1",d=>yScale(d[0]))
-              .attr("x2",d=>xScale(d.data.session))
-              .attr("y2",d=>yScale(d[1]))
-              .attr("stroke-width",d=>d[1]>d[0]?Math.min(5,xScale.bandwidth()/5):0)
-           
-        svg.selectAll(".layer").each(function(p,j,g){
-            d3.select(this)
-            .selectAll("rect")
-            .each(function(d,e,f){
-                tippy(this,{content:
-                    liste.get(d.data.session).user_nom+"<p>"+p.key+":"+(d[1]-d[0])+"</p>", arrow: true,})
-                }
-            )            
-        })
-       tippy(".evtline", {content:"tipp",
-           async onShow(tip) {
-               //console.log('tip',liste.get(d3.select(tip.reference).datum().data.session))
-               tip.setContent(liste.get(d3.select(tip.reference).datum().data.session).loads)
-       }})
-       
-            svg.append("g")
-            .attr("class", "axis axis--x")
-            .attr("transform", "translate(0," + (height+5) + ")")
-            .call(xAxis);
+      //fonction de tracé du graphique
+      const trace=svg1=>{
+          //on  filtre l'affichage des types          
+          tdim.filter(type=>svg1.selectAll("[notselected]").data().indexOf(type)==-1)
+                    
+          var stack = d3.stack()
+          .keys(stackKey.sort())
+          .order(d3.stackOrderNone)
+          // .order(d3.stackOrderDescending)
+          .offset(d3.stackOffsetNone);
+  
+          var layers= stack(zdata())
+          //data.sort(function(a, b) { return b.s - a.s; });
+          xScale.domain(zdata().map(function(d) { return d.session; }));
+          //recherche du y max
+          var maxi=0
+          layers.forEach(l=>{let m=d3.max(l,function(d){return d[1]?d[1]:0}); if (m>maxi) maxi=m})
+          yScale.domain([0, maxi]).nice();
+          
+          svg1.select("#stackedchart").remove()
+          
+          var svg=svg1.append("g").attr("id","stackedchart")
+          var layer = svg.selectAll(".layer")
+              .data(layers)
+              .enter().append("g")
+              .attr("class", "layer")
+              .style("fill", function(d, i) { return color(d.key); })
+        
+            var rectSel=layer.selectAll("g.evt")
+                .data(function(d) { return d; })
+                .enter()
+                .append("g").attr("class",'evt')
+                .classed('noboucle',d=>boucles[d.data.session]==null)
+                .classed('boucle',d=>boucles[d.data.session]!=null)
+            rectSel.append("rect")
+                  //.classed('noboucle',d=>boucles[d.data.session]==null)
+                  //.classed('boucle',d=>boucles[d.data.session]!=null)
+                .attr("x", function(d) { return xScale(d.data.session); })
+                .attr("y", function(d) { return yScale(d[1]); })
+                .attr("height", function(d) { return yScale(d[0]) - yScale(d[1]); })
+                .attr("width", xScale.bandwidth())
+            rectSel.append("line")
+                .attr("class","evtline")
+                .attr("x1",d=>xScale(d.data.session))
+                .attr("y1",d=>yScale(d[0]))
+                .attr("x2",d=>xScale(d.data.session))
+                .attr("y2",d=>yScale(d[1]))
+                .attr("stroke-width",d=>d[1]>d[0]?Math.min(5,xScale.bandwidth()/5):0)
+             
+          svg.selectAll(".layer").each(function(p,j,g){
+              d3.select(this)
+              .selectAll("rect")
+              .each(function(d,e,f){
+                  tippy(this,{content:
+                      liste.get(d.data.session).user_nom+"<p>"+p.key+":"+(d[1]-d[0])+"</p>", arrow: true,})
+                  }
+              )            
+          })
+         tippy(".evtline", {content:"tipp",
+             async onShow(tip) {
+                 //console.log('tip',liste.get(d3.select(tip.reference).datum().data.session))
+                 tip.setContent(liste.get(d3.select(tip.reference).datum().data.session).loads)
+         }})
+            
+            
+              svg.append("g")
+              .attr("class", "axis axis--x")
+              .attr("transform", "translate(0," + (height+5) + ")")
+              .call(xAxis);
 
-            svg.append("g")
-            .attr("class", "axis axis--y")
-            .attr("transform", "translate(0,0)")
-            .call(yAxis);                           
+              svg.append("g")
+              .attr("class", "axis axis--y")
+              .attr("transform", "translate(0,0)")
+              .call(yAxis);       
+              
+    
+      }
+      //tracé de la legende
+            const legend = svg => {
+                const g = svg
+                    .attr("font-family", "sans-serif")
+                    .attr("font-size", 10)
+                    .attr("text-anchor", "start")
+                  .selectAll("g.legend")
+                  .data(stackKey)
+                  .enter().append("g").attr("class","legend")
+                    .attr("transform", (d, i) => `translate(${width},${i * 20})`);
+
+                g.append("rect")
+                    .attr("x", 0)
+                    .attr("width", 19)
+                    .attr("height", 19)
+                    .attr("fill", color)
+                    //.attr("selected",1)
+                    .on("click",function(d,i){
+                        const el=d3.select(this)
+                        el.attr("notselected",el.attr("notselected")?null:1)
+                        trace(svg)
+                        }
+                    )
+
+                g.append("text")
+                    .attr("x", 24)
+                    .attr("y", 9.5)
+                    .attr("dy", "0.35em")
+                    .text(d => d)
+                    .attr("selected",true)
+              }
+            trace(svg)
+            legend(svg)
     }
 }
